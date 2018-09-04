c538d321b1335f9b17c967c222c10f6c5829784d
Mitch-Heath <42604088+Mitch-Heath@users.noreply.github.com>
2018-09-04 16:39:25 +0100

    Add files via upload

---
+++ styling.js
@@ -0,0 +1,10 @@
+function hover(input) {
+    input.style.color = "blue";
+    input.width= 200;
+    input.height= 200;
+}
+function hoverOff(input) {
+    input.style = ("color:white");
+    input.width= 100;
+    input.height= 100;
+}
